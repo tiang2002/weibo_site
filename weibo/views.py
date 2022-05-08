a6e17f9f8b623358e833db89c63b941226666123
tiang2002 <1657389281@qq.com>
2022-05-08 18:17:36 +0800

    change9--points and lines

--- weibo/views.py
+++ weibo/views.py
@@ -37,12 +37,12 @@ def china(request):
 
 def points(request):
     ids = []
-    with open(PROJECT_ROOT + r'\weibo\all_id_list.csv') as file:
+    with open(PROJECT_ROOT + r'/weibo/all_id_list.csv') as file:
         reader = csv.reader(file)
         for each in reader:
             ids.append(each[0])
     relations = []
-    with open(PROJECT_ROOT + r'\weibo\weibo_forward_list6.csv') as file:
+    with open(PROJECT_ROOT + r'/weibo/weibo_forward_list6.csv') as file:
         reader = csv.reader(file)
         reader.__next__()
         for each in reader:
